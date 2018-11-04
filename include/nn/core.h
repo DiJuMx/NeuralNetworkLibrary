9a9ab29d51bf427dab54a37f2cf6a243467999ae
Daniel Moran <dijumx@gmail.com>
2018-11-04 21:16:32 +0000

    Simplified and consolidated units and layers
    Units (Neurons) and Layers (Arrays of Neurons) have been consolidated
    into a single "Bundle" structure. A "Bundle" is a collection of a single
    type of neuron and functions like a layer would.

--- include/nn/core.h
+++ include/nn/core.h
@@ -17,55 +17,41 @@ enum nnStatusCodes {
         NN_SUCCESS = 0x0,       /**< @brief Generic success statuscode */
 };
 
-/** @brief The nnUnit structure represents a single neuron, or unit.
+/** @brief A container to hold the arrays associated with a nnBundle's output
  */
-struct nnUnit {
-        /** The units inputs variable should point to the array of input pointers in the layer */
-        double ** inputs;
-        /** The units output variable should point to the appropriate element in the layer output array */
-        double *  output;
-        /** The units gInputs variable should point to the array of gradient input pointers in the layer */
-        double ** gInputs;
-        /** The unit gOutput should point to the appropriate element in the layer gradient output array */
-        double *  gOutput; 
-
-        /** Function pointer for the forward pass or activation function */
-        int (*fPtrForward)(struct nnUnit * unit);
-        /** Function pointer for the backward pass or gradient calculation function */
-        int (*fPtrBackward)(struct nnUnit * unit);
-
-        /** This pointer contains any additional functionality required for correct operation of the unit */
-        void * extra;
+struct nnBundleOutput {
+        int     number;
+        float * values;
+        float * gradients;
 };
 
-
-/** @brief The nnLayer structure acts as a wrapper around a collection of nnUnits.
+/** @brief A Bundle is the equivalent of a single layer of a specific neuron type.
  */
-struct nnLayer {
-        /** The array of neural units within the layer */
-        struct nnUnit* units;
-        /** An array of pointers which reference the outputs in the previous layers  */
-        double ** inputs;
-        /** An array of values the units in the layer should populate with activations */
-        double *  output;
-        /** An array of pointers which reference the gradients of outputs in the previous layers */
-        double ** gInputs;
-        /** An array of values accumulating the gradients from the subsequent layers */
-        double *  gOutput;
+struct nnBundle {
+        /** A container holding the bundle output */
+        struct nnBundleOutput   output;
 
-        /** The number of elements in the input and gInput arrays */
-        int numInputs;
-        /** The number of elements in the input and gInput arrays */
-        int numOutputs;
-        /** The number of units within the layer */
-        int numUnits;
+        /** An array of pointers to the outputs of previous bundles */
+        struct nnBundleOutput** inputs;
+
+        /** Number of elements in the inputs array */
+        int                     numInputBundles;
+
+        /** Non-common data */
+        void *                  data;
+
+        /** Forward pass function */
+        enum nnStatusCode (*forward)(struct nnBundle * bundle);
+
+        /** Backward pass function */
+        enum nnStatusCode (*backward)(struct nnBundle * bundle);
 };
 
-/** @brief The nnNetwork structure groups together all of the nnLayers within the model
+/** @brief The nnNetwork structure groups together all of the nnLayers within the model.
  */
 struct nnNetwork {
-        /** Array of layers in the network. */
-        struct nnLayer * layers;
+        /** An array of the layers within the entire network */
+        struct nnBundle * layers;
 };
 
 
